0a6a20e942119d0e3c822e914c9c8e41f0fbc244
LisaTostrams <lisamc.tostrams@gmail.com>
2023-05-25 14:52:09 +0200

    init and added all photos

---
+++ app.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 25 14:13:40 2023
+
+@author: tostraml
+"""
+
+import dash
+import dash_bootstrap_components as dbc
+from dash import html
+
+
+app = dash.Dash(
+    external_stylesheets=[dbc.themes.QUARTZ],
+    meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"}
+    ],
+)
+
+app.layout=  html.Div([
+    html.H4('Welkom bij de SPA planten BINGO!'),
+    dbc.Row(
+            [
+                dbc.Col(html.Div("One of three columns"),xs=6),
+                dbc.Col(html.Div("One of three columns"),xs=6),
+            ]
+            )
+])
+
+app.run(host='0.0.0.0', port=8050)
