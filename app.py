228d0f41afb2336dd08cddaa9dc41640e19c5ef7
LisaTostrams <lisamc.tostrams@gmail.com>
2023-05-30 12:02:57 +0200

    team input

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ Created on Thu May 25 14:13:40 2023
 
 import dash
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+
 from dash import html, dcc
 
 
@@ -19,17 +21,33 @@ app = dash.Dash(
 
 app.layout=  html.Div([
     html.H4('Welkom bij de SPA planten BINGO!'),
+    dcc.Location(id='url'),
     dbc.Row(dcc.Input(
             id="team",
             type='text',
             placeholder="Laat hier ÉÉN teamlid jullie team naam invoeren",
+            debounce=True
         )),
+    html.Div(id='content',children=[
     dbc.Row(
             [
-                dbc.Col(html.Div("One of two columns"),xs=6),
-                dbc.Col(html.Div("One of two columns"),xs=6),
+                dbc.NavLink(
+                    "Team naam",id='team_link', href="/team_naam", className='nav-link')
+                # dbc.Col(html.Div("One of two columns"),xs=6),
+                # dbc.Col(html.Div("One of two columns"),xs=6),
             ]
-            )
+            )])
 ])
+
+@app.callback(
+    Output("team_link", "children"),
+    Output("team_link", "href"), 
+    Input('team','value'),
+)
+def cb_render(team_naam):
+    if team_naam==None:
+        return 'Voer een naam in','/'
+    return f'Hier is de link voor je team {team_naam}', f'/{team_naam}'
+
 
 app.run(host='0.0.0.0', port=8050)
