0b1a5862bb91f80861ecef0d087035438c9ee80a
LisaTostrams <lisamc.tostrams@gmail.com>
2023-06-01 15:07:58 +0200

    more info log

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from whitenoise import WhiteNoise
 app = dash.Dash(
     external_stylesheets=[dbc.themes.QUARTZ],
     use_pages=True,
-    pages_folder='pages',
+    pages_folder='',
     meta_tags=[
         {"name": "viewport", "content": "width=device-width, initial-scale=1"}
     ],
@@ -27,7 +27,7 @@ app = dash.Dash(
 
 server = app.server
 server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
-
+app.pages_folder='static/pages'
 
 app.layout=  html.Div([
     html.H2('Welkom bij de SPA planten BINGO!'),
